6386acce2aef77521be73c09d41bc7dcffc138cc
Ali-Rida Ammar <sam.onx@outlook.com>
2024-02-12 12:20:24 +0100

    snabb uppdate

--- README.md
+++ README.md
@@ -1,32 +1,6 @@
 # dsd project repo
 PRIVATE School-Project about Distributed Systems.
 
-För att köra detta projekt behöver du Node.js och npm installerat på din dator. Gör såhär:
-
-1. Klona repot först om du inte har gjort det. (Öppna din terminal, cmd, windows powershell eller liknande)
-använd kommando:
-
-git clone https://github.com/FreeAhuraBird/dsd_repo
-
-2. Navigera nu till projektets katalog med kommando:
-
-cd [där du sparat projektet]
-
-3. Installera nu Node.js med kommando:
-
-npm install
-
-4. Du kan nu köra applikationen med följade kommando:
-
-npm start
-
-
-Applikationen är nu tillgänglig i din webbläsare på http://localhost:3000.
-
-
-
-
-
 ## Usage (for dsd_project)
 
 Dont forget to change to feature branch before making changes to code.
--- server/dsd_repo-frontend/src/components/PhoneForm.js
+++ server/dsd_repo-frontend/src/components/PhoneForm.js
@@ -6,6 +6,7 @@ eller skapa en ny användare med Register knappen. Än sålänge har vi inte anv
 för att införa login. Det är enkelt att implimentera men jag tycker att det inte är bra för vår uppgift. */
 
 function PhoneForm() {
+    // det här hanterar "show / hide" den ögonknappen alltså.
     const [showPassword, setShowPassword] = useState(false);
 
     const togglePasswordVisibility = () => {
@@ -22,7 +23,8 @@ function PhoneForm() {
                 <input type={showPassword ? "text" : "password"} id="password" placeholder="Password" />
                 <button onClick={togglePasswordVisibility} className="toggle-password" type="button">
                     {showPassword ? (
-                        // Super long copy & paste länk från w3 schools.
+                        // Super long copy & paste länk från w3. Det är bara så vi ska ta de två ögon knapparna, 
+                        // en för öppet öga en för stängt öga.
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-eye-off"><path d="M17.94 17.94A10 10 0 0 0 21 12c-1.74-4.41-7-7-9-7-1.26 0-2.4.38-3.4 1.02"/><path d="M1 1l22 22"/><path d="M9.9 4.24A9.958 9.958 0 0 0 3 12c1.74 4.41 7 7 9 7 1.26 0 2.4-.38 3.4-1.02"/><circle cx="12" cy="12" r="3"/></svg>
                     ) : (
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-eye"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"/><circle cx="12" cy="12" r="3"/></svg>
